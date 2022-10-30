09aba9b51ae62a631c455c791e343e5d2b912780
nicolasvhs <nicolasvale97@gmail.com>
2022-10-30 18:55:14 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1IVhtPEr8Nl1t46KAhq5JOHw')
+  const price = await stripe.prices.retrieve('price_1LyRZ6IlhxQvZmnDqt0IsMpv')
 
   const product = {
     priceId: price.id,
